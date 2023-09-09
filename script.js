97771ba91e14156204ef9f0864c772fa7ec14f84
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-09 13:24:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your code here.
+const container=document.getElementsByClassName("items")[0];
+const items=document.querySelectorAll(".item");
+items.forEach((item)=>{
+	item.setAttribute("draggable","true")
+	
+})
+items.forEach((item)=>{
+	item.addEventListener("dragend",(e)=>{
+		let target=e.target;
+		console.log(target);
+	})
+})
+
